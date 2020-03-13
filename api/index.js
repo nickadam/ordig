663b4ecc5bca2d53d2ea7e2c4b50cc74d81badb0
Nick Vissari <nvissari@hcpss.org>
2020-03-13 15:22:38 +0000

    Add documentation

--- api/index.js
+++ api/index.js
@@ -1,1 +1,15 @@
 'use strict'
+const express = require('express')
+const sqlite3 = require('sqlite3').verbose()
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./openapi.yml')
+
+const app = express()
+
+app.use(express.json())
+
+// host swagger doc
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+app.listen(9000, () => console.log('API listening on port 9000'))
