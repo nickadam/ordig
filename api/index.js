66642076d167e5967e2744bfaed66ef4dc542eeb
Nick Vissari <nvissari@hcpss.org>
2020-03-14 11:16:32 +0000

    Require API keys

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 'use strict'
+const fs = require('fs')
 const express = require('express')
 const sqlite3 = require('sqlite3').verbose()
 const swaggerUi = require('swagger-ui-express')
@@ -12,6 +13,8 @@ const wg_ip = process.env.WG_IP || '10.10.10.1'
 const wg_endpoint = process.env.WG_ENDPOINT || 'wg.example.com'
 const wg_port = process.env.WG_PORT || '51280'
 const wg_allowed = process.env.WG_ALLOWED || '10.0.0.0/8, 172.16.0.0/12, 192.168.0.0/24'
+const wg_api_key = fs.readFileSync('/run/secrets/wg_api_key')
+const wg_client_api_key = fs.readFileSync('/run/secrets/wg_client_api_key')
 
 const app = express()
 
@@ -20,6 +23,46 @@ app.use(express.json())
 // host swagger doc
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
+// require API keys
+const server_url_re = /^\/api\/v[0-9]\/server/
+const client_url_re = /^\/api\/v[0-9]\/client/
+app.use((req, res, next) => {
+  if(server_url_re.test(req.path)){
+    if(req.get('Authorization')){
+      const auth = req.get('Authorization').split(' ')
+      if(auth[1]){
+        const token = auth[1]
+        if(token == wg_api_key){
+          next()
+        }else{
+          res.status(401).send('invalid token')
+        }
+      }else{
+        res.status(401).send('invalid token')
+      }
+    }else{
+      res.status(401).send('invalid token')
+    }
+  }else if(client_url_re.test(req.path)){
+    if(req.get('Authorization')){
+      const auth = req.get('Authorization').split(' ')
+      if(auth[1]){
+        const token = auth[1]
+        if(token == wg_client_api_key){
+          next()
+        }else{
+          res.status(401).send('invalid token')
+        }
+      }else{
+        res.status(401).send('invalid token')
+      }
+    }else{
+      res.status(401).send('invalid token')
+    }
+  }
+})
+
+
 // generate or get server keypair
 init_server(db, (err, server_keypair) => {
   if(err){
@@ -27,12 +70,22 @@ init_server(db, (err, server_keypair) => {
     process.exit(1)
   }
 
-  // test sending keypair
-  app.use('/api/v1/server', (req, res) => {
+  // send server config
+  app.use('/api/v1/server/config', (req, res) => {
     res.send({
       ip: wg_ip,
       port: wg_port,
       private_key: server_keypair.private_key
+    })
+  })
+
+  // test client
+  app.use('/api/v1/client', (req, res) => {
+    res.send({
+      wg_endpoint: wg_endpoint,
+      wg_port: wg_port,
+      wg_allowed: wg_allowed,
+      public_key: server_keypair.public_key
     })
   })
 })
--- api/test.js
+++ api/test.js
@@ -1,5 +1,8 @@
 'use strict'
+const http = require('http')
+const fs = require('fs')
 
+// test keypair generator
 const keypair = require('./generate_keypair')()
 if(!(keypair.private_key.length === 44 && keypair.public_key.length === 44)){
   console.error('generate_keypair failed', keypair.private_key.length)
@@ -7,6 +10,7 @@ if(!(keypair.private_key.length === 44 && keypair.public_key.length === 44)){
   console.log('generate_keypair passed')
 }
 
+// test database server init
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database(':memory:')
 const init_server = require('./init_server')
@@ -17,4 +21,55 @@ init_server(db, (err, keypair) => {
     console.log('server_keypair passed')
   }
   db.close()
+})
+
+// test client api key
+const wg_client_api_key = fs.readFileSync('/run/secrets/wg_client_api_key')
+const options = {
+  headers: {
+    Authorization: 'Bearer ' + wg_client_api_key
+  }
+}
+http.get('http://127.0.0.1:9000/api/v1/client', options, res => {
+  res.on('data', d => {
+    if(JSON.parse(d.toString()).public_key.length === 44){
+      console.log('client api key permit passed')
+    }else{
+      console.error('client api key permit failed')
+    }
+  })
+})
+options.headers.Authorization = 'Bearer 12345'
+http.get('http://127.0.0.1:9000/api/v1/client', options, res => {
+  res.on('data', d => {
+    if(d.toString() === 'invalid token'){
+      console.log('client api key reject passed')
+    }else{
+      console.error('client api key reject failed')
+    }
+  })
+})
+
+
+//test server api key
+const wg_api_key = fs.readFileSync('/run/secrets/wg_api_key')
+options.headers.Authorization = 'Bearer ' + wg_api_key
+http.get('http://127.0.0.1:9000/api/v1/server/config', options, res => {
+  res.on('data', d => {
+    if(JSON.parse(d.toString()).private_key.length === 44){
+      console.log('server api key permit passed')
+    }else{
+      console.error('server api key permit failed')
+    }
+  })
+})
+options.headers.Authorization = 'Bearer 12345'
+http.get('http://127.0.0.1:9000/api/v1/server/config', options, res => {
+  res.on('data', d => {
+    if(d.toString() === 'invalid token'){
+      console.log('server api key reject passed')
+    }else{
+      console.error('server api key reject failed')
+    }
+  })
 })
