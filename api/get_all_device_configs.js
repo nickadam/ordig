fc3b31f965d6b55da6b6e96768be41ccaa452b73
Nick Vissari <nvissari@hcpss.org>
2020-03-14 14:46:32 +0000

    Send all configs to the server

--- api/get_all_device_configs.js
+++ api/get_all_device_configs.js
@@ -1,5 +1,13 @@
 'use strict'
 
 module.exports = (db, next) => {
-  next()
+  db.all('SELECT * FROM clients', (err, rows) => {
+    if(err){
+      next(err)
+    }else{
+      next(null, rows.map(i => {
+        return {ip: i.ip, public_key: JSON.parse(i.keypair).public_key}
+      }))
+    }
+  })
 }
