d21e359b229c6d83cfd6fdbf8767a4abf1cb206c
Nick Vissari <nvissari@hcpss.org>
2020-03-15 19:53:45 -0400

    Add wait for API

--- server/wg_server.sh
+++ server/wg_server.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+server="$(grep api_url server/config.json | awk -F \" '{print $4}')"
+
+while [ "$(curl -s -o /dev/null -w "%{http_code}" $server/public/works)" != 200 ]
+do
+  sleep 1
+done
+
 while true
 do
   python3 daemon.py &>> daemon.log
--- start.sh
+++ start.sh
@@ -4,10 +4,22 @@ cd /opt/ordig
 
 docker-compose up -d
 
-sleep 10
-
 cd server
+
+server="$(grep api_url config.json | awk -F \" '{print $4}')"
+
+while [ "$(curl -s -o /dev/null -w "%{http_code}" $server/public/works)" != 200 ]
+do
+  echo "Waiting for API..."
+  sleep 5
+done
 
 nohup ./wg_server.sh &
 
+echo ""
+echo ""
 echo "Server started!"
+echo ""
+echo "Copy /opt/ordig/wg.ps1 to your windows clients and run as administrator"
+echo ""
+echo ""
